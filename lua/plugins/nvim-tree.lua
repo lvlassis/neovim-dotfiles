fbd689521803e1b01c0779fe985b17c5dc023768
lvinicius <lvinicius@tce.go.gov.br>
2026-09-14 10:16:40 -0300

    fix: corrige algo na nvim tree para windows

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -41,12 +41,16 @@ end
 
 
 -- Auto Close Nvim Tree
+local function is_floating(winnr)
+  return vim.api.nvim_win_get_config(winnr).relative ~= ""
+end
+
 local function tab_win_closed(winnr)
   local api = require"nvim-tree.api"
   local tabnr = vim.api.nvim_win_get_tabpage(winnr)
   local bufnr = vim.api.nvim_win_get_buf(winnr)
   local buf_info = vim.fn.getbufinfo(bufnr)[1]
-  local tab_wins = vim.tbl_filter(function(w) return w~=winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
+  local tab_wins = vim.tbl_filter(function(w) return w~=winnr and not is_floating(w) end, vim.api.nvim_tabpage_list_wins(tabnr))
   local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
   if buf_info.name:match(".*NvimTree_%d*$") then            -- close buffer was nvim tree
     -- Close all nvim tree on :q
@@ -58,7 +62,8 @@ local function tab_win_closed(winnr)
       local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
       if last_buf_info.name:match(".*NvimTree_%d*$") then       -- and that buffer is nvim tree
         vim.schedule(function ()
-          if #vim.api.nvim_list_wins() == 1 then                -- if its the last buffer in vim
+          local real_wins = vim.tbl_filter(function(w) return not is_floating(w) end, vim.api.nvim_list_wins())
+          if #real_wins == 1 then                              -- if its the last buffer in vim
             vim.cmd "quit"                                        -- then close all of vim
           else                                                  -- else there are more tabs open
             vim.api.nvim_win_close(tab_wins[1], true)             -- then close only the tab
